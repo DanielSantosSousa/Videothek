a32040ea69b8b293f987b60c0828a83e02d37f13
Daniel Santos Sousa <Daniel_SantosSousa@sluz.ch>
2021-04-28 11:52:11 +0200

    update getAll in Loan

--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -30,7 +30,26 @@ class loan
         $statement = $pdo->prepare('SELECT * FROM loans ORDER BY date');
         $statement->execute();
         $result = $statement->fetchAll();
-        return $result;
+
+        $editedResult = [];
+        foreach ($result as $loan){
+            $movieArrayLocation = 'fk_movieid';
+            $membershipArrayLocation = 'fk_membershipstatusid';
+            $movie = new Movie();
+            $movie->getById($loan[$movieArrayLocation]);
+            $membership = new MembershipStatus();
+            $membership->getById($loan[$membershipArrayLocation]);
+            $loan[$movieArrayLocation] = $movie->title;
+            $loan[$membershipArrayLocation] = $membership->title;
+            $loan += ['expectedReturn' => self::calcReturnDate($loan['date'], $membership)];
+            $editedResult[] = $loan;
+        }
+        return $editedResult;
+    }
+
+    public static function calcReturnDate($date, MembershipStatus $membership)
+    {
+        return date_add(date_create($date), date_interval_create_from_date_string($membership->extraDays + 30 . ' days'));
     }
 
     public function create() {
